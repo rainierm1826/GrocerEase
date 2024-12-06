4b7a8bc61ab3fde1aa819c3f7e56aa433ddca789
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2024-12-06 12:20:31 +0800

    Update order.js

--- frontend/src/api/order.js
+++ frontend/src/api/order.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const order = axios.create({
-  baseURL: "http://localhost:5000/order",
+  baseURL: "https://grocerease-backend-oif1.onrender.com",
   withCredentials: true,
 });
 
