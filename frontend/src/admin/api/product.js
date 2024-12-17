6e0b2bc9cb32393d545fe8826db883e364def7fc
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2024-12-17 14:08:41 +0800

    Update product.js

--- frontend/src/admin/api/product.js
+++ frontend/src/admin/api/product.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const product = axios.create({
-  baseURL: "http://localhost:5000/product",
+  baseURL: "https://final-project-grocerease.onrender.com/product",
   withCredentials: true,
 });
 
