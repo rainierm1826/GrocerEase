307261d61874fe8a5c1ce53f1711fe80276332a7
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2024-12-17 14:11:19 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ app.use(express.json({ limit: "10mb" }));
 // cors
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://final-project-grocerease.onrender.com",
     credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
@@ -47,7 +47,7 @@ app.use(
       ttl: 14 * 24 * 60 * 60,
     }),
     cookie: {
-      secure: false,
+      secure: true,
       maxAge: 14 * 24 * 60 * 60,
       saveUninitialized: false,
     },
