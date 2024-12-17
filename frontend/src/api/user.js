0ebf68e900464a7ea2daad1abda613071fc630b2
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2024-12-17 14:06:36 +0800

    Update user.js

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const user = axios.create({
-  baseURL: "http://localhost:5000/user",
+  baseURL: "https://final-project-grocerease.onrender.com",
   withCredentials: true,
 });
 
