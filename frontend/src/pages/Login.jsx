f8d7ae535fa46aaaf4da8606962e07918f818b6d
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2024-12-17 11:24:15 +0800

    upd

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -31,7 +31,8 @@ const Login = () => {
           <button
             className="flex items-center border-[1px] border-gray-200 py-2 px-5 rounded-xl w-2/3 hover:bg-gray-100 transition-colors ease-in duration-150"
             onClick={() =>
-              (window.location.href = "http://localhost:5000/auth/google")
+              (window.location.href =
+                "https://grocerease-backend-7b5o.onrender.com/auth/google")
             }
           >
             <img src={googleIcon} alt="" className="w-5 h-5" />
@@ -40,7 +41,8 @@ const Login = () => {
           <button
             className="flex items-center border-[1px] border-gray-200 py-2 px-5 rounded-xl w-2/3 hover:bg-gray-100 transition-colors ease-in duration-150"
             onClick={() =>
-              (window.location.href = "http://localhost:5000/auth/facebook")
+              (window.location.href =
+                "https://grocerease-backend-7b5o.onrender.com/auth/facebook")
             }
           >
             <img src={facebookIcon} alt="" className="w-5 h-5" />
